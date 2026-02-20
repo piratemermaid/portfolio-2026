1b5566cb63499b3581df8aa03dae96bd75eb37ec
Ariel <thepiratemermaid4@gmail.com>
2026-02-20 16:57:52 -0700

    design welcome text

--- src/components/layout/WelcomeBanner.tsx
+++ src/components/layout/WelcomeBanner.tsx
@@ -3,15 +3,18 @@ import { Box, Typography } from "@mui/material";
 export const WelcomeBanner = () => {
   return (
     <Box sx={{ p: 4, borderRadius: 2, textAlign: "center" }}>
-      <Typography variant="h1">The Pirate Mermaid</Typography>
+      <Typography variant="h2" component="h1">
+        The Pirate Mermaid
+      </Typography>
       <Typography
         variant="h4"
-        color="text.secondary"
+        component="h2"
+        color="primary.main"
         sx={{ fontFamily: "Domine, serif" }}
       >
         Developer & Designer
       </Typography>
-      <Typography variant="body1" sx={{ mt: 2 }}>
+      <Typography variant="subtitle1" sx={{ mt: 2 }}>
         {`I've combined my love for coding and design to create beautiful and 
           functional websites and web applications.`}
       </Typography>
